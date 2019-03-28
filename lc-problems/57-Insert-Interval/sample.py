f0e83855149bf61b9e5185d65331c7c544b3047e
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-03-29 07:56:06 +0800

    Submit Problem #57.

---
+++ lc-problems/57-Insert-Interval/sample.py
@@ -0,0 +1,12 @@
+def insert(self, intervals, newInterval):
+    s, e = newInterval.start, newInterval.end
+    left, right = [], []
+    for i in intervals:
+        if i.end < s:
+            left += i,
+        elif i.start > e:
+            right += i,
+        else:
+            s = min(s, i.start)
+            e = max(e, i.end)
+    return left + [Interval(s, e)] + right
