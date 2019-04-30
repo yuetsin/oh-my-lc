cc214f3b5f421bd2e0ceef72926dd3c85ca1720e
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-04-30 13:58:06 +0800

    Submit Problem #102.

---
+++ lc-problems/102-Binary-Tree-Level-Order-Traversal/mine.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    max_level = -1
+
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        global_lst = []
+
+        def goLevel(node: TreeNode, level: int):
+            if node == None:
+                return
+            while level > self.max_level:
+                global_lst.append([])
+                self.max_level += 1
+            global_lst[level].append(node.val)
+            goLevel(node.left, level + 1)
+            goLevel(node.right, level + 1)
+        goLevel(root, 0)
+        return global_lst
