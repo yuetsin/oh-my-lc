0bd4c0276992c23d414cb81d122cd4d674e0be0d
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-05-13 20:06:34 +0800

    Submit Problem #109.

---
+++ lc-problems/109-Convert-Sorted-List-to-Binary-Search-Tree/mine_badsmell.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, num):
+        if not num:
+            return None
+
+        mid = len(num) // 2
+
+        root = TreeNode(num[mid])
+        root.left = self.sortedArrayToBST(num[:mid])
+        root.right = self.sortedArrayToBST(num[mid+1:])
+
+        return root
+
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        result = []
+        while head != None:
+            result.append(head.val)
+            head = head.next
+        return self.sortedArrayToBST(result)
