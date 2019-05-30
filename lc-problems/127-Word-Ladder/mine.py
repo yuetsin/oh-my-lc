5a641a9ee9ede4a24039aee330e697095cce397a
yuetsin <akaza_akari@sjtu.edu.cn>
2019-05-30 08:24:05 +0800

    Submit Problem #127.

---
+++ lc-problems/127-Word-Ladder/mine.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+
+        wordList = set(wordList)
+        res = []
+        layer = {}
+        layer[beginWord] = [[beginWord]]
+
+        while layer:
+            newlayer = collections.defaultdict(list)
+            for w in layer:
+                if w == endWord:
+                    res.extend(k for k in layer[w])
+                else:
+                    for i in range(len(w)):
+                        for c in 'abcdefghijklmnopqrstuvwxyz':
+                            neww = w[:i]+c+w[i+1:]
+                            if neww in wordList:
+                                newlayer[neww] += [j+[neww] for j in layer[w]]
+
+            wordList -= set(newlayer.keys())
+            layer = newlayer
+
+        return 0 if len(res) == 0 else len(res[0])
