7ee87130ddf780b39be1cdbbce4c06cbad26fa37
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-11 10:21:55 +0800

    Submit Problem #149.

---
+++ lc-problems/149-Max-Points-on-a-Line/mine_unfinished.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        # --------------------------------------------------------------------
+        # NOTE: input types have been changed on April 15, 2019.
+        # Please reset to default code definition to get new method signature.
+        # -----------------------------------------------------------------------
+        #
+        # I suppose the original input must be awful.
+        pass
