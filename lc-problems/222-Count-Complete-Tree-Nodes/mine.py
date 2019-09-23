e03f851f22d8d21b33c7b52c5b791c9fc6cfb877
yuetsin <akaza_akari@sjtu.edu.cn>
2019-09-23 11:41:30 +0800

    [add] submit problem #222

---
+++ lc-problems/222-Count-Complete-Tree-Nodes/mine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
