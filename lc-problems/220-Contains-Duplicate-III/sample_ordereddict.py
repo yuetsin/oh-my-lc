86c9122e8ab8b0cc4c738d9edb86eae0d5dcdbc5
yuetsin <akaza_akari@sjtu.edu.cn>
2019-09-23 09:14:44 +0800

    [add] submit problem #220

---
+++ lc-problems/220-Contains-Duplicate-III/sample_ordereddict.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if k < 1 or t < 0:
+            return False
+        dic = collections.OrderedDict()
+        for n in nums:
+            key = n if not t else n // t
+            for m in (dic.get(key - 1), dic.get(key), dic.get(key + 1)):
+                if m is not None and abs(n - m) <= t:
+                    return True
+            if len(dic) == k:
+                dic.popitem(False)
+            dic[key] = n
+        return False
