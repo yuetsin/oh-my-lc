b8bd05c942b835b2d4b3b106fdc6f3e1ccad25a4
yuetsin <akaza_akari@sjtu.edu.cn>
2019-10-09 20:55:13 +0800

    Update not_solving_the_problem.py

--- lc-problems/289-Game-of-Life/not_solving_the_problem.py
+++ lc-problems/289-Game-of-Life/not_solving_the_problem.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-
-
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
@@ -10,3 +7,13 @@ class Solution:
         # and it could be terminated any time.
         # so why am i wasting my time
         # solving these leetcode free problems?
+        #
+        # what if i made a small world
+        # when i was programming?
+        # what if there's someone in the small world
+        # realizes its existense is so shallow
+        #
+        # and just meant for nothing, merely for
+        # an online programming website?
+        #
+        # i can't make it. i'll stop solving this problem.
