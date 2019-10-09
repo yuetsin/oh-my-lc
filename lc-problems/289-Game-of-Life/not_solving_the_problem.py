74cf94a19d92ba4c0e1307dd7a5e3f27f1986201
yuetsin <akaza_akari@sjtu.edu.cn>
2019-10-09 16:15:08 +0800

    [add] #289 myth

---
+++ lc-problems/289-Game-of-Life/not_solving_the_problem.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        # Maybe i'm living in such a game of life now.
+        # and it could be terminated any time.
+        # so why am i wasting my time
+        # solving these leetcode free problems?
