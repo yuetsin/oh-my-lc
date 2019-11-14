077b2517ce02ea56bd42e1a71f7e2aa830181873
yuetsin <akaza_akari@sjtu.edu.cn>
2019-11-14 14:19:11 +0800

    [add] submit problem #397

---
+++ lc-problems/397-Integer-Replacement/mine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache(maxsize=None)
+    def integerReplacement(self, n: int) -> int:
+        if n == 1:
+            return 0
+        if n % 2 == 0:
+            return 1 + self.integerReplacement(n // 2)
+        else:
+            return 1 + min(self.integerReplacement(n + 1), self.integerReplacement(n - 1))
---
+++ lc-problems/397-Integer-Replacement/notes.md
@@ -0,0 +1,17 @@
+# 397
+
+## Integer Replacement
+
+Runtime: 28 ms, faster than 96.65% of Python3 online submissions for Integer Replacement.
+
+Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Integer Replacement.
+
+I’ll say
+
+```python
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+```
+
+is perfect.
