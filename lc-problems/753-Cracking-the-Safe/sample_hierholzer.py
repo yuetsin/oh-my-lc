e014dd27bf62cd61b5f05f2b6a5d2b7d71faed82
yuetsin <akaza_akari@sjtu.edu.cn>
2020-02-22 15:55:48 +0800

    [add] submit problem #753
    破题！

---
+++ lc-problems/753-Cracking-the-Safe/sample_hierholzer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+class Solution(object):
+    def crackSafe(self, n, k):
+        seen = set()
+        ans = []
+
+        def dfs(node):
+            for x in map(str, range(k)):
+                nei = node + x
+                if nei not in seen:
+                    seen.add(nei)
+                    dfs(nei[1:])
+                    ans.append(x)
+
+        dfs("0" * (n-1))
+        return "".join(ans) + "0" * (n-1)
